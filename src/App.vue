f8275e8dc1fca6ba913d8a4a00686a851bf83fa4
Shahin <ahmshahin5353@gmail.com>
2020-05-31 22:48:33 +0600

    error fix

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@ export default {
     setResults(results){
       this.weather = results
     },
-    dateBuilder(results){
+    dateBuilder(){
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
